1df165bc9112b2ace4cf0746f555a433e222616f
Chan <chan15tw@gmail.com>
2014-10-29 11:39:15 +0800

    update jQuery to newest version

--- inc.php
+++ inc.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="css/fantasybasketball.css" />
-<link rel="stylesheet" href="css/ui-lightness/jquery-ui-1.10.3.custom.min.css" />
+<link rel="stylesheet" href="css/jquery-ui.min.css" />
 <link rel="stylesheet" href="css/bootstrap.css" />
 <link rel="stylesheet" href="css/bootstrap-responsive.css" />
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-<script src="js/jquery.min.js"></script>
-<script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+<script src="js/jquery.js"></script>
+<script src="js/jquery-ui.min.js"></script>
 <script src="js/jquery.tablesorter.js"></script>
 <script src="js/bootstrap.js"></script>
