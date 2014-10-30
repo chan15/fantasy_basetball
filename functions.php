4c4656fab77385b6f033bd5d2b483026a4d08548
Chan <chan15tw@gmail.com>
2014-10-30 13:58:33 +0800

    adjust curl function

--- functions.php
+++ functions.php
@@ -2,16 +2,19 @@
 
 // Get page content from cURL
 function getUrl($url) {
-    $ch = curl_init(); 
-    $opt = array(
-        CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_URL            => $url,
-		CURLOPT_HEADER         => false
-    );
-
-    curl_setopt_array($ch, $opt);
+    $result = '';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     $result = curl_exec($ch);
-    curl_close($ch); 
+
+    if (false === $result) {
+        $result = curl_error($ch);
+    }
+
+    curl_close($ch);
 
     return $result;
 }
